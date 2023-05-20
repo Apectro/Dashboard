652b39a79208360f3bcb39c19f35fd50870c43e5
sonqs <49322724+sonqs@users.noreply.github.com>
2023-05-20 13:27:15 +0200

    Deploy.sh Refactor

--- deploy.sh
+++ deploy.sh
@@ -9,17 +9,11 @@ fi
 # Create the lock file
 touch deploy.lock
 
-# Fetch the latest changes from the master branch
-git fetch origin master
-
-# Reset the code to the latest commit on the master branch
-git reset --hard origin/master
-
 # Move to the backend directory
 cd ../back-end
 
 # Install dependencies if node_modules doesn't exist or package-lock.json has changed
-if [ ! -d "node_modules" ] || [ "$(git diff --name-only HEAD~1 package-lock.json)" != "" ]; then
+if [ ! -d "../back-end/node_modules" ] || [ "$(git diff --name-only HEAD~1 ../back-end/package-lock.json)" != "" ]; then
   npm ci
 else
   echo "Skipping npm install for backend"
@@ -35,7 +29,7 @@ node dist/index.js &
 cd ../front-end
 
 # Install dependencies if node_modules doesn't exist or package-lock.json has changed
-if [ ! -d "node_modules" ] || [ "$(git diff --name-only HEAD~1 package-lock.json)" != "" ]; then
+if [ ! -d "../front-end/node_modules" ] || [ "$(git diff --name-only HEAD~1 ../front-end/package-lock.json)" != "" ]; then
   npm ci
 else
   echo "Skipping npm install for frontend"
@@ -45,4 +39,4 @@ fi
 npm start
 
 # Remove the lock file when the deployment is complete
-rm ../deploy.lock
+rm deploy.lock
