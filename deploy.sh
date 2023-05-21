2f585ba6c1f5eaedd8ca0c10554d798c3ba0747e
sonqs <49322724+sonqs@users.noreply.github.com>
2023-05-21 18:15:25 +0200

    deploy.sh refactor

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,16 @@ fi
 # Create the lock file
 touch deploy.lock
 
+# Function to shut down running processes
+shutdown_processes() {
+  echo "Shutting down processes..."
+  pkill -f "node dist/index.js"
+  pkill -f "npm start"
+}
+
+# Trap the script exit to ensure processes are shut down
+trap shutdown_processes EXIT
+
 # Move to the backend directory
 cd back-end
 
@@ -22,21 +32,29 @@ fi
 # Build the backend
 tsc
 
-# Start the backend server
+# Start the backend server in the background
 node dist/index.js &
 
 # Move to the frontend directory
 cd ../front-end
 
-# Install dependencies if node_modules doesn't exist or package-lock.json has changed
-if [ ! -d "../front-end/node_modules" ] || [ "$(git diff --name-only HEAD~1 ../front-end/package-lock.json)" != "" ]; then
-  npm install
+# Check if the front-end server is already running
+if pgrep -f "npm start" >/dev/null; then
+  echo "Front-end server is already running. Skipping npm start."
 else
-  echo "Skipping npm install for frontend"
+  # Install dependencies if node_modules doesn't exist or package-lock.json has changed
+  if [ ! -d "../front-end/node_modules" ] || [ "$(git diff --name-only HEAD~1 ../front-end/package-lock.json)" != "" ]; then
+    npm install
+  else
+    echo "Skipping npm install for frontend"
+  fi
+
+  # Start the frontend development server in the background
+  npm start &
 fi
 
-# Start the frontend development server
-npm start
+# Wait for user input to exit the script
+read -r -p "Press any key to stop the script..."
 
 # Remove the lock file when the deployment is complete
 rm deploy.lock
